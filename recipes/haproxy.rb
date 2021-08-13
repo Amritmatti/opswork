8fa8f41b504419d30ff102566a26a6d60d9028a2
Amritmatti <30613161+Amritmatti@users.noreply.github.com>
2021-08-13 16:55:08 +0530

    Update haproxy.rb

--- recipes/haproxy.rb
+++ recipes/haproxy.rb
@@ -36,50 +36,50 @@ end
 #   timeout 70
 # end
 
-if node[:haproxy][:ssl_enabled]
-search('aws_opsworks_app', 'deploy:true').each do |app|
-  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.crt" do
-      mode 0600
-      source 'haproxy/ssl.key.erb'
-      variables :key => app[:ssl_configuration][:certificate]
-      only_if do
-        app[:enable_ssl] && app[:ssl_configuration][:certificate]
-      end
-  end
-
-  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.key" do
-    mode 0600
-    source 'haproxy/ssl.key.erb'
-    variables :key => app[:ssl_configuration][:private_key]
-    only_if do
-      app[:enable_ssl] && app[:ssl_configuration][:private_key]
-    end
-  end
-
-  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.ca" do
-    mode 0600
-    source 'haproxy/ssl.key.erb'
-    variables :key => app[:ssl_configuration][:chain]
-    only_if do
-      app[:enable_ssl] && app[:ssl_configuration][:chain]
-    end
-  end
-
-  Chef::Log.info("debugging haproxy recipe...")
-
-  ssl_pem = app[:ssl_configuration][:private_key] + "\n" + app[:ssl_configuration][:certificate] + "\n" + app[:ssl_configuration][:chain]
-
-  template "#{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem" do
-    mode 0600
-    source 'haproxy/ssl.key.erb'
-    variables :key => ssl_pem
-    only_if do
-      app[:enable_ssl] && app[:ssl_configuration][:certificate] && app[:ssl_configuration][:private_key] && app[:ssl_configuration][:chain]
-    end
-  end
-end
-end
-
+#if node[:haproxy][:ssl_enabled]
+#search('aws_opsworks_app', 'deploy:true').each do |app|
+#  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.crt" do
+#      mode 0600
+#      source 'haproxy/ssl.key.erb'
+#      variables :key => app[:ssl_configuration][:certificate]
+#      only_if do
+#        app[:enable_ssl] && app[:ssl_configuration][:certificate]
+#      end
+#  end
+#
+#  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.key" do
+#    mode 0600
+#    source 'haproxy/ssl.key.erb'
+#    variables :key => app[:ssl_configuration][:private_key]
+#    only_if do
+#      app[:enable_ssl] && app[:ssl_configuration][:private_key]
+#    end
+#  end
+#
+#  template "#{node[:haproxy][:dir]}/ssl/#{app[:domains].first}.ca" do
+#    mode 0600
+#    source 'haproxy/ssl.key.erb'
+#    variables :key => app[:ssl_configuration][:chain]
+#    only_if do
+#      app[:enable_ssl] && app[:ssl_configuration][:chain]
+#    end
+#  end
+#
+#  Chef::Log.info("debugging haproxy recipe...")
+#
+#  ssl_pem = app[:ssl_configuration][:private_key] + "\n" + app[:ssl_configuration][:certificate] + "\n" + app[:ssl_configuration][:chain]
+#
+#  template "#{node[:haproxy][:dir]}/ssl/#{node[:haproxy][:cert]}.pem" do
+#    mode 0600
+#    source 'haproxy/ssl.key.erb'
+#    variables :key => ssl_pem
+#    only_if do
+#      app[:enable_ssl] && app[:ssl_configuration][:certificate] && app[:ssl_configuration][:private_key] && app[:ssl_configuration][:chain]
+#    end
+#  end
+#end
+#end
+#
 execute "echo 'checking if haproxy is not running - if so start it'" do
   not_if "pgrep haproxy"
   notifies :start, "service[haproxy]"
